4f8379322f117f1aae99e862f741a2a2ca2c7c8c
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-09-29 18:39:18 +0300

    Reapply "Add REST framework integration"
    This reverts commit 5352f9c8c1ced410c0b9d8a2eb810ef6062edf59.

--- events_planning_django/events_planning_django/settings.py
+++ events_planning_django/events_planning_django/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app"
+    "app",
+    "rest_framework",
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -72,6 +74,18 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
 
 WSGI_APPLICATION = "events_planning_django.wsgi.application"
 
